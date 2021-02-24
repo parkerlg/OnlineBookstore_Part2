2eae50cf3b2af9761607bd456a23cbce8385a8ee
parkerlg <69094049+parkerlg@users.noreply.github.com>
2021-02-24 10:04:16 -0700

    Initial

---
+++ OnlineBookstore/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineBookstore.Models;
+using OnlineBookstore.Models.ViewModels;
+
+namespace OnlineBookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private IBookstoreRepository _repository;
+
+        public int PageSize = 5;
+
+        public HomeController(ILogger<HomeController> logger, IBookstoreRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public IActionResult Index(int page = 1)
+        {
+            return View(
+                new BookListViewModel
+                {
+                    Books = _repository.Books.
+                        OrderBy(p => p.BookId)
+                        .Skip((page - 1) * PageSize)
+                        .Take(PageSize)
+                    ,
+                    PagingInfo = new PagingInfo
+                    {
+                        CurrentPage = page,
+                        ItemsPerPage = PageSize,
+                        TotalNumItems = _repository.Books.Count()
+                    }
+    });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OnlineBookstore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using OnlineBookstore.Models.ViewModels;
+
+namespace OnlineBookstore.Infrastructure
+{
+    [HtmlTargetElement("ul", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper (IUrlHelperFactory hp)
+        {
+            urlHelperFactory = hp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext viewContext { get; set; }
+
+        public PagingInfo PageModel { get; set; }
+
+        public string PageAction { get; set; }
+
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(viewContext);
+
+
+            TagBuilder result = new TagBuilder("ul");
+
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder listitem = new TagBuilder("li");
+                listitem.AddCssClass("nav-item");
+                TagBuilder tag = new TagBuilder("a");
+                tag.Attributes["href"] = urlHelper.Action(PageAction, new { page = i });
+                //tag.AddCssClass("nav-link");
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
+                tag.InnerHtml.Append(i.ToString());
+                listitem.InnerHtml.AppendHtml(tag);
+                result.InnerHtml.AppendHtml(listitem);
+            }
+            output.Content.AppendHtml(result.InnerHtml);
+
+        
+        }
+    }
+
+}
